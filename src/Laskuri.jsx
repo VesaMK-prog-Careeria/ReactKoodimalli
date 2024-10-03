7312bb8844aaaa447c5a0ce1ee7baabd21558173
Vesa Kalliomäki <vesa.kalliomaki@student.careeria.fi>
2024-10-03 11:47:48 +0300

    laskuri päivityksiä

--- src/Laskuri.jsx
+++ src/Laskuri.jsx
@@ -9,11 +9,16 @@ const [luku, setLuku] = useState(0)
   return (
     <>
         <h3>{luku}</h3>
-        <button onClick={() => setLuku(luku + 1)}>+</button>
+        {luku < 10 && <button onClick={() => setLuku(luku + 1)}>+</button>}
+        {luku > 9 && <button disabled>+</button>}
         <button onClick={() => setLuku(luku - 1)}>-</button>
         <button onClick={() => setLuku(0)}>nollaa</button>
-
+        <br /> <br />
+        <input type='number' value={luku} onChange={(e) => setLuku(parseInt(e.target.value))} />
         <button onClick={props.huomio}>Huomio</button>
+
+        {/* ternary operator tapa esittää ehtolause */}
+        <h5>{luku > 9 ? 'Pääsit kymppiin asti!' : 'Sinulla on vielä matkaa kymppiin asti'}</h5>
 
     </>
   )
